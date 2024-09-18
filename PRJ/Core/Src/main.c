3352e14708f9a85a0594bbd7adc6ce459670fe40
Thien Cao <thien.caothuan@hcmut.edu.vn>
2024-09-18 14:12:59 +0700

    ex 6 update

--- PRJ/Core/Src/main.c
+++ PRJ/Core/Src/main.c
@@ -92,7 +92,41 @@ int main(void)
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
-  {
+  { //ex6
+        	  HAL_GPIO_WritePin ( LED_1_GPIO_Port , LED_1_Pin , GPIO_PIN_SET ) ;
+	          HAL_GPIO_WritePin ( LED_2_GPIO_Port , LED_2_Pin , GPIO_PIN_SET ) ;
+	  	  	  HAL_GPIO_WritePin ( LED_3_GPIO_Port , LED_3_Pin , GPIO_PIN_SET ) ;
+	  	  	  HAL_GPIO_WritePin ( LED_4_GPIO_Port , LED_4_Pin , GPIO_PIN_SET ) ;
+	  	      HAL_GPIO_WritePin ( LED_5_GPIO_Port , LED_5_Pin , GPIO_PIN_SET ) ;
+	  	  	  HAL_GPIO_WritePin ( LED_6_GPIO_Port , LED_6_Pin , GPIO_PIN_SET ) ;
+	  	  	  HAL_GPIO_WritePin ( LED_7_GPIO_Port , LED_7_Pin , GPIO_PIN_SET ) ;
+	  	      HAL_GPIO_WritePin ( LED_8_GPIO_Port , LED_8_Pin , GPIO_PIN_SET ) ;
+	  	  	  HAL_GPIO_WritePin ( LED_9_GPIO_Port , LED_9_Pin , GPIO_PIN_SET ) ;
+	  	  	  HAL_GPIO_WritePin ( LED_10_GPIO_Port , LED_10_Pin , GPIO_PIN_SET ) ;
+	  	      HAL_GPIO_WritePin ( LED_11_GPIO_Port , LED_11_Pin , GPIO_PIN_SET ) ;
+	  	  	  HAL_GPIO_WritePin ( LED_12_GPIO_Port , LED_12_Pin , GPIO_PIN_SET ) ;
+
+	  	  	  HAL_Delay(1000);
+
+        	  HAL_GPIO_WritePin ( LED_1_GPIO_Port , LED_1_Pin , GPIO_PIN_RESET ) ;
+	          HAL_GPIO_WritePin ( LED_2_GPIO_Port , LED_2_Pin , GPIO_PIN_RESET ) ;
+	  	  	  HAL_GPIO_WritePin ( LED_3_GPIO_Port , LED_3_Pin , GPIO_PIN_RESET ) ;
+	  	  	  HAL_GPIO_WritePin ( LED_4_GPIO_Port , LED_4_Pin , GPIO_PIN_RESET ) ;
+	  	      HAL_GPIO_WritePin ( LED_5_GPIO_Port , LED_5_Pin , GPIO_PIN_RESET ) ;
+	  	  	  HAL_GPIO_WritePin ( LED_6_GPIO_Port , LED_6_Pin , GPIO_PIN_RESET ) ;
+	  	  	  HAL_GPIO_WritePin ( LED_7_GPIO_Port , LED_7_Pin , GPIO_PIN_RESET ) ;
+	  	      HAL_GPIO_WritePin ( LED_8_GPIO_Port , LED_8_Pin , GPIO_PIN_RESET ) ;
+	  	  	  HAL_GPIO_WritePin ( LED_9_GPIO_Port , LED_9_Pin , GPIO_PIN_RESET ) ;
+	  	  	  HAL_GPIO_WritePin ( LED_10_GPIO_Port , LED_10_Pin , GPIO_PIN_RESET ) ;
+	  	      HAL_GPIO_WritePin ( LED_11_GPIO_Port , LED_11_Pin , GPIO_PIN_RESET ) ;
+	  	  	  HAL_GPIO_WritePin ( LED_12_GPIO_Port , LED_12_Pin , GPIO_PIN_RESET ) ;
+
+	  	  	  HAL_Delay(1000);
+
+
+
+
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
